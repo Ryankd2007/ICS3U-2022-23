df580001b4b70c0b12de9c7292c073659efe12bf
Ryan Kakavand <rkakavand@bayviewglen.ca>
2022-09-09 10:05:31 -0400

    git.status

---
+++ Unit 1/src/ExampleTwo.java
@@ -0,0 +1,19 @@
+public class ExampleTwo {
+    public static void main(String[] args) {
+        // we must escape the " inside the round brackest because " quotes have meaning in Java
+        // backslash is used to escape characters
+        System.out.println("Computer Science is the \"Best\" Course ever");
+
+        // there are a small subset of characters that can be escaped.
+        // but the AP exam will only test you on \", \n, and \\
+
+        System.out.println("This is \\\\a test.")
+
+        System.out.println("ABCD\nEFGHIJK")
+    
+
+        
+
+    }
+
+}   
