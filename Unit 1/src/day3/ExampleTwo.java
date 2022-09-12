9481cc22d72d3089a61c811ff1951505c49da319
Ryan Kakavand <rkakavand@bayviewglen.ca>
2022-09-12 15:03:36 -0400

    Fixed comment

---
+++ Unit 1/src/day3/ExampleTwo.java
@@ -0,0 +1,23 @@
+package day3;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        int x = 7;
+        x = x + 1;
+        System.out.println(x);
+
+        int numberOfStudents = 26;
+        numberOfStudents = numberOfStudents + 1;
+        numberOfStudents++;
+
+        System.out.println(numberOfStudents);
+
+        numberOfStudents++;
+        System.out.println(numberOfStudents);
+
+        numberOfStudents = numberOfStudents / 3;
+        numberOfStudents /= 3;
+        
+    }
+
+}
